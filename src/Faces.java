f31adf08de9d8aa3c219a61d3e190a4f157ec8b8
Tobias Estefors <dv13tes@cs.umu.se>
2016-10-13 14:32:50 +0200

    jebjeb

---
+++ src/Faces.java
@@ -0,0 +1,9 @@
+/**
+ *  KONSTANTER
+ */
+public class Faces {
+    public final static int HAPPY=1;
+    public final static int SAD=2;
+    public final static int MISCHIEVOUS=3;
+    public final static int MAD=4;
+}
