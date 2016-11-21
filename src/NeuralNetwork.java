4838b7c0be76e01c849fa0bd4cd9236759d7d5b9
Tobz0r <tobz0r7@gmail.com>
2016-11-21 07:23:06 +0100

    Slängde ihop något för att parsa filerna

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -13,3 +13,8 @@ public class NeuralNetwork {
 
 
 }
+
+/*
+http://softwareengineering.stackexchange.com/questions/289622/is-the-output-of-a-neural-net-supposed-to-have-had-the-activation-function-appli
+https://www8.cs.umu.se/kurser/5DV121/HT15/slides/10%20-%20ANN.pdf
+ */
