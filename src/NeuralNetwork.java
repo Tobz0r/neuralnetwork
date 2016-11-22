18c35bff4a053bb06a26c69222877bc91d9dce13
Thom Renström[filter lfs] <thomrenstrom@hotmail.se>
2016-11-22 06:58:29 +0100

    asd

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -22,6 +22,7 @@ public class NeuralNetwork {
 
         }
         //testa ba skriva ut
+        
         for(int i=0; i < 20; i++){
             for(int j =0; j < 20; j++){
                 System.out.println(trainingNetwork[i][j]);
