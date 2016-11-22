65e40eac0c295fe85f1b9bdfc47c918706fc53e6
Tobz0r <tobz0r7@gmail.com>
2016-11-22 06:11:34 +0100

    jebane

--- src/Image.java
+++ src/Image.java
@@ -32,12 +32,14 @@ public class Image {
     }
 
     public void printMatrix(){
-        for(int i=0; i < matrix.length;i++){
-            for(int j=0; j < matrix[0].length;j++){
-                System.out.print(matrix[i][j]+" ");
+        try {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
+        }catch (NullPointerException e){}
     }
 
 }
