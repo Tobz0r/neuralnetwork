9fae4b5d2187002fe58c45799c559d280ca4e70b
sejiaw <thomrenstrom@hotmail.se>
2016-12-06 11:22:22 +0100

    asd123

--- src/Faces.java
+++ src/Faces.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+/**
+ * Main file for the neural network
+ * A network will be trained with images and answers.
+ * Then be tested on a file with new "faces".
+ * This test will determine how well the neural network works.
+ * @author dv13trm, dv13tes
+ */
 public class Faces {
  //   java Expresseions training-A.txt facit-A.txt test-B.txt > result.txt
 
